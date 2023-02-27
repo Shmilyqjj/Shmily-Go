420a12b771ed23428a07eaaaafe3e9b27d8de6ff
shmilyqjj <710552907@qq.com>
2023-02-27 15:40:07 +0800

    add split str

--- golang_study/basic_grammar/String.go
+++ golang_study/basic_grammar/String.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	strConcat()
+	splitStr()
 }
 
 func strConcat() {
@@ -12,4 +16,15 @@ func strConcat() {
 	var format_str = "i=%d s=%s"
 	var str = fmt.Sprintf(format_str, i, s)
 	fmt.Println(str)
+}
+
+func splitStr() {
+	s := "ba:bbb:aaa"
+	if strings.HasPrefix(s, "ba:") {
+		sp := strings.Split(s, ":")
+		for i := range sp {
+			fmt.Println(sp[i])
+		}
+	}
+
 }
