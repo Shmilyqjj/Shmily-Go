75f07352ac0a7fa4c03c0664b8aa44c1f299e7a3
shmilyqjj <710552907@qq.com>
2024-08-23 16:09:17 +0800

    string func

--- golang_study/basic_grammar/String.go
+++ golang_study/basic_grammar/String.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/sirupsen/logrus"
 	"strings"
+	"unicode/utf8"
 )
 
 func main() {
 	strConcat()
 	splitStr()
 	replaceStr()
+	invalidUtf8String()
 }
 
 func strConcat() {
@@ -35,4 +38,37 @@ func replaceStr() {
 	if strings.ContainsAny(s, "'") {
 		fmt.Println(strings.Replace(s, "'", "\"", -1))
 	}
+}
+
+/*
+*
+无效UTF8字符串处理
+*/
+func invalidUtf8String() {
+	s := "\xcf\xcf067" // invalid UTF-8 string
+	if !utf8.ValidString(s) {
+		logrus.Warn("String is not valid UTF-8, do sanitizeString")
+		s = sanitizeString(s)
+	} else {
+		logrus.Infoln("String is valid UTF-8")
+	}
+	fmt.Println("Cleaned string:", s)
+}
+
+/*
+*
+处理非UTF8字符串 输出处理替换后的
+*/
+func sanitizeString(s string) string {
+	validStr := make([]rune, 0, len(s))
+	for len(s) > 0 {
+		r, size := utf8.DecodeRuneInString(s)
+		if r == utf8.RuneError && size == 1 {
+			validStr = append(validStr, '?')
+		} else {
+			validStr = append(validStr, r)
+		}
+		s = s[size:]
+	}
+	return string(validStr)
 }
